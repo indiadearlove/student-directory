b31aad22c966480042a9c039ef69de82facf4296
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-04 17:45:54 +0000

    Directory update 1

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#first we print the list of students
+
 puts "The students of a Makers Academy cohort"
 puts "-----------"
 puts "Harry Potter"
@@ -6,3 +8,9 @@ puts "Ron Weasley"
 puts "Fred Weasley"
 puts "George Weasley"
 puts "Luna Lovegood"
+
+#finally we print the total number of students
+
+print "Overall, we have "
+print 6
+puts " great students"
